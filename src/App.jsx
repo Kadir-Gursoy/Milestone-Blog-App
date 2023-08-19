37c4f4249a8bc04e0af67f37aa5d6016fcf6351e
Mcclayn798 <mnkmcc9@gmail.com>
2023-08-19 23:08:02 +0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+
+import AppRouter from "./route/AppRouter"
+
+
+function App() {
+ 
+
+  return (
+
+
+    
+    <AppRouter/>
+
+  
+
+  )
+}
+
+export default App
---
+++ src/route/AppRouter.jsx
@@ -0,0 +1,17 @@
+import { BrowserRouter as Router, Routes } from "react-router-dom"
+import Appbar from "../components/Appbar";
+
+
+ const AppRouter = () => {
+    return(
+        
+        <Router>
+            <Appbar/>
+            <Routes>
+
+            </Routes>
+        </Router>
+    )
+}
+
+export default AppRouter
